96539dbaa118fbe95e6f55d5e4e3b93fa7e96e15
Roberto Moran <remorandev@gmail.com>
2020-09-17 22:27:57 +0200

    proyecto base

---
+++ src/main/java/dev/remoran/clone/reddit/SpringRedditCloneApplication.java
@@ -0,0 +1,13 @@
+package dev.remoran.clone.reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedditCloneApplication.class, args);
+    }
+
+}
