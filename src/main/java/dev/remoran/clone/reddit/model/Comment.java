10c092e5e39b6c36cdaf389dcf6b8df0587c36b7
Roberto Moran <remorandev@gmail.com>
2020-09-19 10:32:01 +0200

    data base models

---
+++ src/main/java/dev/remoran/clone/reddit/model/Comment.java
@@ -0,0 +1,32 @@
+package dev.remoran.clone.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String text;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
---
+++ src/main/java/dev/remoran/clone/reddit/model/Post.java
@@ -0,0 +1,39 @@
+package dev.remoran.clone.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long postId;
+    @NotBlank(message = "Post Name cannot be empty or null")
+    private String postName;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount = 0;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+}
